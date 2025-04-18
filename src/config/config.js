1a93edca5c594e4a009429502019715aef77b8dc
陈永昌 <chenyongchang@bwcj.com>
2025-04-18 18:11:08 +0800

    fix: config

--- src/config/config.js
+++ src/config/config.js
@@ -8,13 +8,13 @@ import jiuxishibajian from "./article/jiuxishibajian";
 import malingudao from "./article/malingudao";
 import meiwugudao from "./article/meiwugudao";
 import zoumagang from "./article/zoumagang";
-const OSS_PATH = "https://cyc-save.oss-cn-shanghai.aliyuncs.com";
+const OSS_PATH = "https://clc-tourism-mp.oss-cn-beijing.aliyuncs.com/mp";
 export default [
   {
     id: 1,
     title: "水桶岙海岸线徒步｜浙版麦理浩径，探寻山海间的绮丽之约",
     content: shuitongao,
-    cover: OSS_PATH + "/clc/shuitongao.jpg",
+    cover: OSS_PATH + "/shuitongao.jpg",
     position: {
       text: "水桶岙",
     },
@@ -23,16 +23,16 @@ export default [
     id: 2,
     title: "探秘四明山：诗意山水间，镌刻灵魂的难忘之旅",
     content: simingshan,
-    cover: OSS_PATH + "/clc/simingshan.jpg",
+    cover: OSS_PATH + "/simingshan.jpg",
     position: {
-      text: "水桶岙",
+      text: "四明山",
     },
   },
   {
     id: 3,
     title: "国家地理推荐｜徽杭古道轻装穿越",
     content: weihangAncientRoad,
-    cover: OSS_PATH + "/clc/weihangAncientRoad.jpg",
+    cover: OSS_PATH + "/weihangAncientRoad.jpg",
     position: {
       text: "徽杭古道",
     },
@@ -41,7 +41,7 @@ export default [
     id: 4,
     title: "绍兴千年盐帮古道徒步 山水相逢，邂逅一场穿越时光的自然盛宴",
     content: yanbangAncientRoad,
-    cover: OSS_PATH + "/clc/yanbangAncientRoad.jpg",
+    cover: OSS_PATH + "/yanbangAncientRoad.jpg",
     position: {
       text: "绍兴",
     },
@@ -50,7 +50,7 @@ export default [
     id: 5,
     title: "竹韵溪声·寻迹章里古道",
     content: zhangliAncientRoad,
-    cover: OSS_PATH + "/clc/zhangliAncientRoad.jpg",
+    cover: OSS_PATH + "/zhangliAncientRoad.jpg",
     position: {
       text: "章里古道",
     },
@@ -59,7 +59,7 @@ export default [
     id: 6,
     title: "九溪十八涧：山水灵韵处，一径入江南",
     content: jiuxishibajian,
-    cover: OSS_PATH + "/clc/jiuxishibajian.jpg",
+    cover: OSS_PATH + "/jiuxishibajian.jpg",
     position: {
       text: "九溪十八涧",
     },
@@ -68,7 +68,7 @@ export default [
     id: 7,
     title: "陆羽问茶寻古道，南浔枕水听流年",
     content: chaxungudao,
-    cover: OSS_PATH + "/clc/chaxungudao.jpg",
+    cover: OSS_PATH + "/chaxungudao.jpg",
     position: {
       text: "茶寻古道",
     },
@@ -77,7 +77,7 @@ export default [
     id: 8,
     title: "桐庐马岭古道：走进富春山居的梦幻徒步之旅",
     content: malingudao,
-    cover: OSS_PATH + "/clc/malingudao.jpg",
+    cover: OSS_PATH + "/malingudao.jpg",
     position: {
       text: "马岭古道",
     },
@@ -86,7 +86,7 @@ export default [
     id: 9,
     title: "梅坞苔阶藏鹤迹，九寨分镜落江南",
     content: meiwugudao,
-    cover: OSS_PATH + "/clc/meiwugudao.png",
+    cover: OSS_PATH + "/meiwugudao.png",
     position: {
       text: "梅坞古道",
     },
@@ -95,7 +95,7 @@ export default [
     id: 10,
     title: "走马岗：千年榧林秘境，越中山水传奇",
     content: zoumagang,
-    cover: OSS_PATH + "/clc/zoumagang.jpg",
+    cover: OSS_PATH + "/zoumagang.jpg",
     position: {
       text: "走马岗",
     },
